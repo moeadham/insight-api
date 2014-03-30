200471acf69a7830554ffcda646786f33e625002
Moe Adham <moeadham@gmail.com>
2014-03-30 04:28:59 -0400

    [WEB] Enable web ports

--- insight.js
+++ insight.js
@@ -78,15 +78,25 @@ require('./config/express')(expressApp, historicSync, peerSync);
 //Bootstrap routes
 require('./config/routes')(expressApp);
 
+//SSL
+var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
+//var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+//var credentials = {key: privateKey, cert: certificate};
+
 // socket.io
 var server = require('http').createServer(expressApp);
+var secure_server = require('https').createServer(credentials, expressApp);
 var ios = require('socket.io').listen(server);
 require('./app/controllers/socket.js').init(expressApp, ios);
 
 //Start the app by listening on <port>
-server.listen(config.port, function(){
+server.listen(80, function(){
     console.log('insight server listening on port %d in %s mode', server.address().port, process.env.NODE_ENV);
 });
+
+/*secure_server.listen(443, function(){
+    console.log('insight server listening on port %d in %s mode', server.address().port, process.env.NODE_ENV);
+});*/
 
 //expose app
 exports = module.exports = expressApp;
